2547a5320de6fd08605caf247da16db5cee49d76
Akashioreki12 <akramtalibi1902@gmail.com>
2023-11-25 12:11:11 +0100

    first commit

---
+++ app/(Onboarding)/slide1.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const slide1 = () => {
+  return (
+    <View className="flex-1 items-center justify-center  bg-green-300">
+      <Text>slide1</Text>
+    </View>
+  );
+}
+
+export default slide1
